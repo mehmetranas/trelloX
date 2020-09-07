cacc6979902f3f8128776f1fc518711a5b9437f1
mehmetranas <mhmtranas@gmail.com>
2020-09-08 00:00:50 +0300

    added readme file

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,11 @@ import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
+  if (window) {
+    window.console.log = function () {};
+  }
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
